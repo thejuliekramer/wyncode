ad595963588fe0b1a71d5c75b557f15aa11c565c
Julie Kramer <thejuliekramer@gmail.com>
2014-05-27 14:32:09 -0400

    Update error_exception.rb

--- error_exception.rb
+++ error_exception.rb
@@ -1,5 +1,3 @@
-error_exception.rb
-
 ## This would be the error class section of the program.
 
 class OhNoYouDidnt < Exception
@@ -7,25 +5,25 @@ class OhNoYouDidnt < Exception
 end
 
 class PrettyStandardError < StandardError
-	puts "It's ok, I know you didn't mean it."
+		
 end
 
 def add_one(number)
 	begin number.respond_to? :+
-		raise OhNoYouDidnt
+		raise OhNoYouDidnt, "The building is on fire!"
 	end
 	number + 1
 end
 
 def add_two(number)
 	begin number.respond_to? :+
-		raise PrettyStandardError
+		raise PrettyStandardError, "It's ok, I know you didn't mean it."
 	end
 	number + 2
 end
 
-# This is testing OhNoYouDidnt but I'm too scared to run this code so I commented it out.
-# add_one (nil)
+# This is testing OhNoYouDidnt 
+add_one (nil)
 
 # This is testing PrettyStandardError
 add_two (nil)
